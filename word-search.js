63f64cbc9fd304a694dae7e061e3e1d4fd4bbf46
Tim Creasy <timcreasy@me.com>
2016-08-26 09:36:48 -0500

    Bonus #1

--- word-search.js
+++ word-search.js
@@ -1,10 +1,12 @@
+#!/usr/bin/env node
+
 const { createReadStream } = require('fs');
 const es = require('event-stream');
 const limitTen = require('./limit-ten');
 
 const DICTIONARY = '/usr/share/dict/words';
 
-createReadStream(DICTIONARY)
+process.stdin
   .pipe(es.split('/n'))
   .pipe(es.map((word, cb) => cb(null, word.toLowerCase() + '\n')))
   .pipe(limitTen)
